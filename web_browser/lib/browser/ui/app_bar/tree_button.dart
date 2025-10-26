90fdae8906d21a33f162e667922d625dfa58af84
Yasahi-021 <210383787+Yasahiro-021@users.noreply.github.com>
2025-10-26 11:42:44 +0900

    refactor: TreeButtonのUIを改善し、カラースキームを適用

--- web_browser/lib/browser/ui/app_bar/tree_button.dart
+++ web_browser/lib/browser/ui/app_bar/tree_button.dart
@@ -11,30 +11,36 @@ class TreeButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    //カラースキームの取得
+    final colors = Theme.of(context).colorScheme;
+
+    // ルートノードの監視
     final rootNode = ref.watch(rootNodeNotifierProvider);
 
     return Container(
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
       child: Column(
         children: [
-          Container(
-            color: Colors.red,
-            child: Text('ツリー', style: Theme.of(context).textTheme.labelSmall),
-          ),
-          Container(
-            color: Colors.blue,
-            child: IconButton(
-              padding: EdgeInsets.zero,
-              icon: const Icon(Icons.account_tree),
-              style: IconButton.styleFrom(
-                foregroundColor: Colors.white,
-              ) ,
-              tooltip: 'ツリー画面へ移動',
-              onPressed: () {
-                // ツリー画面への遷移
-                TreeViewRoute($extra: rootNode).push(context);
-              },
+          Text('ツリー', style: Theme.of(context).textTheme.labelSmall),
+
+          IconButton.filled(
+            iconSize: 35,
+            padding: EdgeInsets.all(2), // 余白を小さく
+            constraints: const BoxConstraints(), // デフォルトの最小サイズ制約を解除
+            icon: const Icon(Icons.account_tree),
+            style: IconButton.styleFrom(
+              foregroundColor: colors.onPrimary,
+              padding: EdgeInsets.all(2), // こちらにも指定
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap, // タップ領域も小さく
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
             ),
+            tooltip: 'ツリー画面へ移動',
+            onPressed: () {
+              // ツリー画面への遷移
+              TreeViewRoute($extra: rootNode).push(context);
+            },
           ),
         ],
       ),
--- web_browser/test/router/router_test.dart
+++ web_browser/test/router/router_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:web_browser/node/mocked_node.dart';
 import 'package:web_browser/router/router.dart';
 
 void main() {
