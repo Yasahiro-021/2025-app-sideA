3c03f1c5182ab297933814283192b577636aba6b
Yasahiro-021 <Yasahiro-021@users.noreply.github.com>
2025-11-23 23:53:40 +0900

    feat: ViewModel層とドメイン層の実装
    - BrowserNodeChildrenNotifier: 子ノードリスト管理
    - BrowserNodeFromPathNotifier: パスからノード取得
    - CurrentPathNotifier: 現在パス管理
    - CurrentNodeProvider: 現在ノード提供
    - RootPathProvider: ルートパス提供
    - CreateNodeUsecase: ノード作成ユースケース
    - NodePathToNodeFunction: パスからNode変換関数

---
+++ web_browser/lib/browser/view_model/notifiers/current_node_provider.dart
@@ -0,0 +1,18 @@
+import 'dart:developer';
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:web_browser/browser/model/browser_node.dart';
+import 'package:web_browser/browser/model/node_path.dart';
+import 'package:web_browser/browser/view_model/notifiers/browser_node_from_path_notifier.dart';
+import 'package:web_browser/browser/view_model/notifiers/current_path_notifier.dart';
+
+part 'current_node_provider.g.dart';
+
+/// 現在のノードを提供する
+@riverpod
+BrowserNode currentNodeProvider(Ref ref){
+  final NodePath currentPath = ref.watch(currentPathProvider);
+  final BrowserNode currentNode = ref.watch(browserNodeFromPathProvider(currentPath));
+  log("current node : $currentNode");
+  return currentNode;
+}
