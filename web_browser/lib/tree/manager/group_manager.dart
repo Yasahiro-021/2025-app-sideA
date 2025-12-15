650e1388af63cf0a07fcf433b7e27470248e3347
Yasahiro-021 <yasahiro021@users.noreply.github.com>
2025-12-15 14:49:38 +0900

    feat: GroupManagerクラスと関連するプロバイダーを追加

---
+++ web_browser/lib/tree/manager/group_manager.dart
@@ -0,0 +1,29 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:web_browser/core/node/node_children.dart';
+import 'package:web_browser/core/node/node_path.dart';
+import 'package:web_browser/core/usecase/children_at_path_manager.dart';
+import 'package:web_browser/tree/model/group.dart';
+
+part 'group_manager.g.dart';
+
+@Riverpod(keepAlive: true)
+class GroupManager extends _$GroupManager {
+  static const int groupPadding = 2;
+
+  @override
+  Group build(NodePath parentPath) {
+    NodeChildren children = ref.watch(childrenAtPathMangerProvider(parentPath));
+    List<Group> childrenGroups = [];
+    for (var childPath in children.children) {
+      Group childGroup = ref.watch(groupManagerProvider(childPath));
+      childrenGroups.add(childGroup);
+    }
+    return Group(
+      path: parentPath,
+      elements: [...children.children],
+      childrenGroup: childrenGroups,
+      width: children.children.length + groupPadding *2, // 両端をプラス
+      treeWidth: childrenGroups.fold(0, (sum, group) => sum + group.width),
+    );
+  }
+}
---
+++ web_browser/lib/tree/manager/group_manager.g.dart
@@ -0,0 +1,101 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'group_manager.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(GroupManager)
+const groupManagerProvider = GroupManagerFamily._();
+
+final class GroupManagerProvider
+    extends $NotifierProvider<GroupManager, Group> {
+  const GroupManagerProvider._({
+    required GroupManagerFamily super.from,
+    required NodePath super.argument,
+  }) : super(
+         retry: null,
+         name: r'groupManagerProvider',
+         isAutoDispose: false,
+         dependencies: null,
+         $allTransitiveDependencies: null,
+       );
+
+  @override
+  String debugGetCreateSourceHash() => _$groupManagerHash();
+
+  @override
+  String toString() {
+    return r'groupManagerProvider'
+        ''
+        '($argument)';
+  }
+
+  @$internal
+  @override
+  GroupManager create() => GroupManager();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(Group value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<Group>(value),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is GroupManagerProvider && other.argument == argument;
+  }
+
+  @override
+  int get hashCode {
+    return argument.hashCode;
+  }
+}
+
+String _$groupManagerHash() => r'b559cc5366ce7843a02a5149b3926a4171ecdddd';
+
+final class GroupManagerFamily extends $Family
+    with $ClassFamilyOverride<GroupManager, Group, Group, Group, NodePath> {
+  const GroupManagerFamily._()
+    : super(
+        retry: null,
+        name: r'groupManagerProvider',
+        dependencies: null,
+        $allTransitiveDependencies: null,
+        isAutoDispose: false,
+      );
+
+  GroupManagerProvider call(NodePath parentPath) =>
+      GroupManagerProvider._(argument: parentPath, from: this);
+
+  @override
+  String toString() => r'groupManagerProvider';
+}
+
+abstract class _$GroupManager extends $Notifier<Group> {
+  late final _$args = ref.$arg as NodePath;
+  NodePath get parentPath => _$args;
+
+  Group build(NodePath parentPath);
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build(_$args);
+    final ref = this.ref as $Ref<Group, Group>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<Group, Group>,
+              Group,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
