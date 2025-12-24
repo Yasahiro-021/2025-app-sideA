40e74500fabbf302f31a8cc22be7f8339d383256
Yasahiro-021 <yasahiro021@users.noreply.github.com>
2025-12-24 09:31:40 +0900

    feat: TreeViewをConsumerWidgetに変更し、InteractiveViewerをTreeMapから移動

--- web_browser/lib/tree/view/tree_view.dart
+++ web_browser/lib/tree/view/tree_view.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:web_browser/tree/view/components/tree_map/tree_map_view.dart';
 import 'package:web_browser/tree/view/components/back_button/back_button.dart';
+import 'package:web_browser/tree/view/tree_settengs_provider.dart';
+import 'package:web_browser/tree/view/tree_settings.dart';
 
 /// ツリー表示画面
-/// 
+///
 /// coreディレクトリにある情報をもとに、ツリーを表示する画面
-class TreeView extends StatelessWidget {
+class TreeView extends ConsumerWidget {
   const TreeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Stack(
+  Widget build(BuildContext context, WidgetRef ref) {
+    // レイアウトの設定を読み込み
+    final TreeSettings settings = ref.watch(treeSettingsProvider);
+
+    // ツリーマップと戻るボタンを配置する
+    return Stack(
       children: [
-        // ツリーマップと戻るボタンを配置する
-        TreeMapView(),
-        Positioned(
-          top: 16,
-          left: 16,
-          child: TreeBackButton(),
+        //インタラクティブの中にツリーマップを配置
+        InteractiveViewer(
+          constrained: false,
+          boundaryMargin: const EdgeInsets.all(double.infinity),
+          minScale: settings.minScale,
+          maxScale: settings.maxScale,
+          panEnabled: true,
+          scaleEnabled: true,
+          child: TreeMapView(),
         ),
+        //戻るボタン
+        Positioned(top: 16, left: 16, child: TreeBackButton()),
       ],
     );
   }
