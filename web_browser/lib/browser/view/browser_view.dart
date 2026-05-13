144326acf71c255dea55bbd5e98096a0f02b1fa3
Yasahi <210383787+Yasahiro-021@users.noreply.github.com>
2026-05-13 11:25:47 +0900

    feat:Browser画面は生成時にTreeIDを受け取るように変更（ホーム画面→ブラウザ画面へのTreeID受け渡し）

--- web_browser/lib/browser/view/browser_view.dart
+++ web_browser/lib/browser/view/browser_view.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:web_browser/browser/view/components/app_bar/browser_app_bar_view.dart';
 import 'package:web_browser/browser/view/components/bottom_bar/browser_bottom_bar_view.dart';
 import 'package:web_browser/browser/view/components/body/browser_web_view/browser_web_view_view.dart';
 import 'package:web_browser/browser/view/components/body/floating_search_bar/floating_search_bar_view.dart';
+import 'package:web_browser/core/providers/current_tree_notifier.dart';
+import 'package:web_browser/core/tree/tree_id.dart';
 
 /// ブラウザ画面のエントリーポイント
-class BrowserView extends StatelessWidget {
-  const BrowserView({super.key});
+class BrowserView extends ConsumerStatefulWidget {
+  const BrowserView({required this.treeId, super.key});
+
+  //表示するツリーのID
+  final TreeId treeId;
+
+  @override
+  ConsumerState<BrowserView> createState() => _BrowserViewState();
+}
+
+class _BrowserViewState extends ConsumerState<BrowserView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(currentTreeProvider.notifier).setCurrentTree(widget.treeId);
+  }
+
+  @override
+  void didUpdateWidget(covariant BrowserView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.treeId != widget.treeId) {
+      ref.read(currentTreeProvider.notifier).setCurrentTree(widget.treeId);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: BrowserAppBarView(),
       body: BrowserWebViewView(),
       floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
